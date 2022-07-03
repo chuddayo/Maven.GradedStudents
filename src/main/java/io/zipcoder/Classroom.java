d3d926965e217bf68076b6b44825570722b5dbc7
Ryan <ryan@zipcodersmbp55.lan>
2022-07-03 14:13:34 -0400

    add/remove students from classroom method

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -1,5 +1,9 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Objects;
+
 public class Classroom {
     public Student[] students;
     public Classroom() {
@@ -17,6 +21,23 @@ public class Classroom {
             sum += student.getAverageExamScore();
         }
         return sum / students.length;
+    }
+    public void addStudent(Student student) {
+        ArrayList<Student> newStudentList = new ArrayList<>(Arrays.asList(students));
+        newStudentList.add(student);
+        students = newStudentList.toArray(new Student[newStudentList.size()]);
+    }
+    public void removeStudent(String firstName, String lastName) {
+        // removal without leaving null values
+        ArrayList<Student> newStudentList = new ArrayList<>(Arrays.asList(students));
+        for (int i = 0; i < newStudentList.size(); i++) {
+            if (newStudentList.get(i).getFirstName().equals(firstName) &&
+                newStudentList.get(i).getLastName().equals(lastName)) {
+                newStudentList.remove(newStudentList.get(i));
+                i--;
+            }
+        }
+        students = newStudentList.toArray(new Student[newStudentList.size()]);
     }
     public Student[] getStudents() {
         return students;
--- src/test/java/io/zipcoder/ClassroomTest.java
+++ src/test/java/io/zipcoder/ClassroomTest.java
@@ -3,6 +3,8 @@ package io.zipcoder;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Arrays;
+
 public class ClassroomTest {
     @Test
     public void getAverageExamScoreTest() {
@@ -22,5 +24,47 @@ public class ClassroomTest {
 
         // Then
         Assert.assertEquals(expected, output);
+    }
+
+    @Test
+    public void addStudentTest() {
+        // Given
+        int maxNumberOfStudents = 1;
+        Classroom classroom = new Classroom(maxNumberOfStudents);
+        Double[] examScores = { 100.0, 150.0, 250.0, 0.0 };
+        Student student = new Student("Leon", "Hunter", examScores);
+
+        // When
+        Student[] preEnrollment = classroom.getStudents();
+        classroom.addStudent(student);
+        Student[] postEnrollment = classroom.getStudents();
+
+        // Then
+        String preEnrollmentAsString = Arrays.toString(preEnrollment);
+        String postEnrollmentAsString = Arrays.toString(postEnrollment);
+        Assert.assertNotEquals(preEnrollmentAsString, postEnrollmentAsString);
+    }
+
+    @Test
+    public void removeStudentTest() {
+        // Given
+        Double[] s1Scores = { 100.0, 150.0 };
+        Double[] s2Scores = { 225.0, 25.0 };
+
+        Student s1 = new Student("student", "one", s1Scores);
+        Student s2 = new Student("student", "two", s2Scores);
+
+        Student[] students = {s1,s2};
+        Classroom classroom = new Classroom(students);
+
+        // When
+        Student[] postEnrollment = classroom.getStudents();
+        classroom.removeStudent("student", "two");
+        Student[] postRemoval = classroom.getStudents();
+
+        // Then
+        String postRemovalAsString = Arrays.toString(postRemoval);
+        String postEnrollmentAsString = Arrays.toString(postEnrollment);
+        Assert.assertNotEquals(postRemovalAsString, postEnrollmentAsString);
     }
 }
