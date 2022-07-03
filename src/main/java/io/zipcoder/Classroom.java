8192e8abbff71d69bfe9b22dac2ade20d8aa7967
Ryan <ryan@zipcodersmbp55.lan>
2022-07-03 13:36:24 -0400

    avg exams core for all students

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -1,4 +1,25 @@
 package io.zipcoder;
 
 public class Classroom {
+    public Student[] students;
+    public Classroom() {
+        this.students = new Student[30];
+    }
+    public Classroom(Student[] students) {
+        this.students = students;
+    }
+    public Classroom(int maxNumberOfStudents) {
+        this.students = new Student[maxNumberOfStudents];
+    }
+    public Double getAverageExamScore() {
+        Double sum = 0.0;
+        for (Student student: students) {
+            sum += student.getAverageExamScore();
+        }
+        return sum / students.length;
+    }
+    public Student[] getStudents() {
+        return students;
+    }
+
 }
--- src/test/java/io/zipcoder/ClassroomTest.java
+++ src/test/java/io/zipcoder/ClassroomTest.java
@@ -1,4 +1,26 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class ClassroomTest {
+    @Test
+    public void getAverageExamScoreTest() {
+        // : Given
+        Double[] s1Scores = { 100.0, 150.0 };
+        Double[] s2Scores = { 225.0, 25.0 };
+
+        Student s1 = new Student("student", "one", s1Scores);
+        Student s2 = new Student("student", "two", s2Scores);
+
+        Student[] students = {s1,s2};
+        Classroom classroom = new Classroom(students);
+        Double expected = 125.0;
+
+        // When
+        Double output = classroom.getAverageExamScore();
+
+        // Then
+        Assert.assertEquals(expected, output);
+    }
 }
